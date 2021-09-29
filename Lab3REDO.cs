b3d736a4f05b60ad3b99aff134e10590b63b1f73
pietryb <90348113+pietryb@users.noreply.github.com>
2021-09-29 09:25:44 -0400

    Add files via upload

---
+++ Lab3REDO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab3REDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a number between 1-100");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 1)
+            {
+                Console.WriteLine($"Odd and {num}");
+
+            }
+            //even
+            else if (num % 2 == 0 && num >= 2 && num <= 25)
+
+            {
+                Console.WriteLine("Even and less tham 25");
+            }
+            else if (num % 2 == 0 && num >= 26 && num <= 60)
+            {
+                Console.WriteLine("Even.");
+
+            }
+            else if (num % 2 == 0 && num > 60)
+            {
+                Console.WriteLine($"{num} and even");
+            }
+            else if (num % 2 == 1 && num > 60)
+            {
+                Console.WriteLine($"{num} and odd");
+            }
+        }
+
+
+    }
+}
